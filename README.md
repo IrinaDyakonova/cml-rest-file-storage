b040eaf0239a84a6c45a3d4aea8cf1f89f3102df
IrinaDyakonova <dyakonova.irina.v@gmail.com>
2020-08-14 18:26:59 +0300

    new project

---
+++ README.md
@@ -0,0 +1,1 @@
+"# cml-rest-file-storage" 
---
+++ src/test/java/cml/rest/file/storage/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package cml.rest.file.storage.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
